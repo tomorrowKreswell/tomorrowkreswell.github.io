a94b05feb64ce891a7040214e4a692992f980781
yancyl <yanerbuer@163.com>
2024-05-28 20:55:53 +0800

    Updates

---
+++ precache-manifest.dd3e9362cb5a770f31c74ea9dd5cbc9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cc7ab8e387d920f6d66",
+    "url": "/static/css/main.4a4b064b.chunk.css"
+  },
+  {
+    "revision": "7cc7ab8e387d920f6d66",
+    "url": "/static/js/main.7cc7ab8e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "477aae9bb290cbe4baba54e2cd73910d",
+    "url": "/index.html"
+  }
+];
