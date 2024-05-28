3b3ec9321e89356261b1bba16a8919d64aa0609a
yancyl <yanerbuer@163.com>
2024-05-28 17:31:12 +0800

    Deploy

---
+++ src/components/Portfolio.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+export default class Porfolio extends Component {
+  handlePortfolioClick(item) {
+    console.log(item);
+    if (item.url) {
+      window.open(item.url, '_blank');
+    } else {
+      alert(`Clicked on: ${item.name}\nDescription: ${item.description}`);
+    }
+  }
+  
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="portfolio">
+      <div className="row">
+        <div className="twelve columns collapsed">
+          <h1>Check Out Some of My Works.</h1>
+          <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+          {
+            resumeData.portfolio && resumeData.portfolio.map((item)=>{
+              return(
+                <div className="columns portfolio-item">
+                  <div className="item-wrap">
+                    <a href="#modal-01">
+                      <img src={`${item.imgurl}`} className="item-img"/>
+                      <div className="overlay">
+                        <div className="portfolio-item-meta">
+                          <h5>{item.name}</h5>
+                          <p>{item.description}</p>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
+                </div>
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+  </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,110 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "YinHui Liu",
+    "role": "Junoir student in ChongQing University",
+    "roleDescription": "热爱科学，跨界，新技术和其他.",
+    // "socialLinks":[
+    //     {
+    //       "name":"linkedin",
+    //       "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+    //       "className":"fa fa-linkedin"
+          
+    //     },
+    //     {
+    //       "name":"github",
+    //       "url":"http://github.com/rbhatia46",
+    //       "className":"fa fa-github"
+    //     },
+    //     {
+    //       "name":"skype",
+    //       "url":"http://twitter.com/rbhatia46",
+    //       "className":"fa fa-twitter"
+    //     }
+    //   ],
+    "aboutme":"I'm now studying in ChongQing University and applying for the opportunity to study in HKUST in Autumn, 2025",
+    "address":"15253559383",
+    "website":"yanerbuer@163.com",
+    "education":[
+      {
+        "UniversityName":"ChongQing University",
+        "specialization":"Information security",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2025",
+        "Achievements":"Weighted Average Mark: 83; Overall GPA: 3.2218 ; 重庆大学优秀共青团干部 ; 计算机学院优秀学生 ; 计算机学院综合奖学金 ; 计算机学院学代会常任学生代表"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"中软国际",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+        "Achievements":"全栈工程师，基于uniapp和springboot搭建程序"
+      },{
+        "CompanyName":"CQU语言科学与语言障碍实验室",
+        "specialization":"student research assistant",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+        "Achievements":"负责组织认知实验，如眼动实验、脑电实验等。"
+      },{
+        "CompanyName":"CQU学生信息安全协会",
+        "specialization":"学生信息安全协会会长",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2023",
+        "Achievements":"组织日常的训练赛、纳新赛，承办unctf重大赛区等。"
+      },{
+        "CompanyName":"CQU学生发展中心",
+        "specialization":"德育发展-文化推广部副部长",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2022",
+        "Achievements":"统筹各部门，负责沟通、对齐工作，以及运营重庆大学学生会微信公众号。"
+      }
+    ],
+    // "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"ILETS (7.0)",
+      },
+      {
+        "skillname":"basic CV(DL) skill"
+      },
+      {
+        "skillname":"basic python/C++/Java/汇编"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"智慧校园生态城————国家级大学生创新创业训练项目",
+        "description":"校园论坛项目，内部集成了匿名聊天、校内跑腿、订水送水等功能，实现最高日活用户2w+。我参与了部分前期的开发（主要是前端界面的搭建），目前负责日常维护。",
+        "imgurl":"images/portfolio/stqhj.png",
+        "url":"images/portfolio/stq.jpg"
+      },
+      {
+        "name":"云上核·焊接装配仿真验真平台————第十届“互联网+”比赛四川赛区入选项目（进行中）",
+        "description":"焊接仿真验证平台，我负责对来自ANSYS的数据进行处理，采用PointCNN进行关键点提取，使得数据清洗后效率更高，并将处理后的数据传入Unreal中。",
+        "imgurl":"images/portfolio/project.jpg",
+        "url":"images/portfolio/hlwk.png"
+      },
+      {
+        "name":"基于D-S证据理论的风险分析————2022重庆大学“srtp”学生科研训练项目良好结题",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.jpg",
+        "url":"images/portfolio/project2.jpg"
+      },
+      {
+        "name":"基于diffusion model的宝可梦图片生成",
+        "description":"对扩散模型的学习项目，使用721张宝可梦图片训练一个能生成宝可梦的模型",
+        "imgurl":"images/portfolio/pockmon.jpg",
+        "url":"images/portfolio/phone.jpg"
+      }
+    ]
+    ,
+    "testimonials":[
+      {
+        "description":"刘崟慧是一个思维敏捷、值得依赖的人，她总能提出很多新的观点。",
+        "name":"重庆大学 康鑫"
+      }
+    ]
+  }
+  
+  export default resumeData
